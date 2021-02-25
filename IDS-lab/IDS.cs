f0e9217a0335aba086439e7dcff36b62b198bd0e
Andrey <airstern@gmail.com>
2021-02-25 15:04:44 +0300

    Simply IDS in C#

---
+++ IDS-lab/IDS.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using NdisApiDotNet;
+using NdisApiDotNetPacketDotNet.Extensions;
+using PacketDotNet;
+using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Timers;
+namespace IDS
+{
+    public class IDS
+    {
+        int ksyn = 0;
+        public void SYNFloodDetector(NdisApi filter, WaitHandle[] waitHandles, IReadOnlyList<NetworkAdapter> networkAdapters, IReadOnlyList<ManualResetEvent> waitHandlesManualResetEvents)
+        {
+            var ndisApiHelper = new NdisApiHelper();
+            var ethRequest = ndisApiHelper.CreateEthRequest();
+            System.Timers.Timer aTimer = new System.Timers.Timer(); //Создаем таймер
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent); //добавляем событие под конец таймера
+            aTimer.Interval = 1000;//1 sec
+            aTimer.Enabled = true;
+            int n = 1;
+
+            while (true)
+            {
+                var handle = WaitHandle.WaitAny(waitHandles);
+                ethRequest.AdapterHandle = networkAdapters[handle].Handle;
+                while (filter.ReadPacket(ref ethRequest) && n == 1)
+                {
+                    var packet = ethRequest.Packet;
+                    var ethPacket = packet.GetEthernetPacket(ndisApiHelper);
+                    if (ethPacket.PayloadPacket is IPv4Packet iPv4Packet)
+                    {
+                        if (iPv4Packet.PayloadPacket is TcpPacket tcpPacket)
+                        {
+                            // Протокол HTTP
+                            if ((tcpPacket.DestinationPort == 80)
+                            || (tcpPacket.SourcePort == 80))
+                            {
+                                // Считывание данных TCP пакета. Получение HTTP пакета
+                                string httpPacket = Encoding.UTF8.GetString(tcpPacket.PayloadData);
+                                if ((httpPacket != "") && (httpPacket != "\0"))
+                                {
+                                    string[] headersHttp = httpPacket.Split(new string[1] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                                    string context = "search";
+                                    // Поиск слова в строке url
+                                    if (headersHttp[0].IndexOf(context) > -1)
+                                    {
+                                        Console.WriteLine($"\r\n{iPv4Packet.SourceAddress}:{tcpPacket.SourcePort} -> {iPv4Packet.DestinationAddress}:{tcpPacket.DestinationPort} | HTTP: {context}");
+                                    }
+                                }
+                            }
+                            // Обнаружение флага SYN в TCP пакете
+                            //string hostName = Dns.GetHostName();
+                            // string myIP = "192.168.31.173";//Dns.GetHostByName(hostName).AddressList[0].ToString();
+                            //IPAddress address = IPAddress.Parse(myIP);
+                            //Console.WriteLine($"\r\naddress: {address}");
+                            //Console.WriteLine($"\r\niPv4Packet: {iPv4Packet.DestinationAddress}");
+                            if (tcpPacket.Syn) //&& iPv4Packet.DestinationAddress == address)
+                            {
+                                ksyn += 1;
+                                Console.WriteLine($"\r\n{iPv4Packet.SourceAddress}:{tcpPacket.SourcePort} -> {iPv4Packet.DestinationAddress}:{tcpPacket.DestinationPort} | Флаг: SYN");
+                                if (ksyn > 500)
+                                {
+                                    Console.WriteLine($"{ksyn} пакетов");
+                                    Console.WriteLine("ПИЗДЕЦ ХУЙНЯ АЛЕ");
+                                    n = 0;
+                                    aTimer.Enabled = false;
+                                }
+                                //if (tcpPacket.Ack == true)
+                                //{
+                                //    Console.WriteLine($"\r\nAck присутствует");
+                                //}
+                            }
+                        }
+                    }
+                    //Отправка пакетов дальше
+                    filter.SendPacket(ref ethRequest);
+                }
+                waitHandlesManualResetEvents[handle].Reset();
+            }
+        }
+        public void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine($"{ksyn} пакетов");
+            ksyn = 0;
+            Console.WriteLine("Таймер закончился! Перезапускаем...");
+        }
+    }
+}
+//перед началом цикла у нас запускается таймер
---
+++ IDS-lab/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using NdisApiDotNet;
+using NdisApiDotNetPacketDotNet.Extensions;
+using PacketDotNet;
+
+namespace IDS
+{
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var filter = NdisApi.Open();
+            if (!filter.IsValid)
+                throw new ApplicationException("Драйвер не найден");
+            Console.WriteLine($"Версия драйвера: {filter.GetVersion()}");
+            // Создать и установить событие для адаптеров
+            var waitHandlesCollection = new List<ManualResetEvent>();
+            // Создание списка сетевых адаптеров
+            var tcpAdapters = new List<NetworkAdapter>();
+            foreach (var networkAdapter in filter.GetNetworkAdapters())
+            {
+                if (networkAdapter.IsValid)
+                {
+                    var success = filter.SetAdapterMode(networkAdapter, 
+                    NdisApiDotNet.Native.NdisApi.MSTCP_FLAGS.MSTCP_FLAG_TUNNEL | 
+                    NdisApiDotNet.Native.NdisApi.MSTCP_FLAGS.MSTCP_FLAG_LOOPBACK_FILTER |
+                    NdisApiDotNet.Native.NdisApi.MSTCP_FLAGS.MSTCP_FLAG_LOOPBACK_BLOCK);
+                    var manualResetEvent = new ManualResetEvent(false);
+                    success &= filter.SetPacketEvent(networkAdapter, manualResetEvent.SafeWaitHandle);
+                    if (success)
+                    {
+                        Console.WriteLine($"Добавлен адаптер: {networkAdapter.FriendlyName}");
+                        // Добавление адаптеров в список
+                        waitHandlesCollection.Add(manualResetEvent);
+                        tcpAdapters.Add(networkAdapter);
+                    }
+                }
+            }
+            var waitHandlesManualResetEvents = waitHandlesCollection.Cast<ManualResetEvent>().ToArray();
+            var waitHandles = waitHandlesCollection.Cast<WaitHandle>().ToArray();
+            // Запуск отдельного потока для анализа пакетов
+            Console.Write("Press ENTER to start");
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            IDS ids = new IDS();
+            if (keyInfo.Key == ConsoleKey.Enter)
+            {
+                var t1 = Task.Factory.StartNew(() => ids.SYNFloodDetector(filter, waitHandles, tcpAdapters.ToArray(), waitHandlesManualResetEvents));
+                Task.WaitAll(t1);
+                Console.Read();
+
+            }
+        }
+
+
+    }
+}
